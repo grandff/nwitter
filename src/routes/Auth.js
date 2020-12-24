bf386c7a46fff276f28c14e280e50e1182480ae7
김정민 <dsaqaz421@naver.com>
2020-12-24 12:03:00 +0900

    2.5 Social Login

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { authService } from "fbase";
+import { authService, firebaseInstance } from "fbase";
 
 const Auth =  () => {
     const [email, setEmail] = useState("");
@@ -39,6 +39,18 @@ const Auth =  () => {
     };
 
     const toggleAccount = () => setNewAccount((prev) => !prev);
+    const onSocialClick = async (event) => {      // sns login
+        const {target : {name}} = event;
+        let provider;
+        if(name === "google"){
+            provider = new firebaseInstance.auth.GoogleAuthProvider();
+        }else if(name === "github"){
+            provider = new firebaseInstance.auth.GithubAuthProvider();
+        }
+
+        const data = await authService.signInWithPopup(provider);
+        console.log(data);
+    }   
 
     return (
     <div>
@@ -50,8 +62,8 @@ const Auth =  () => {
         </form>
         <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
         <div>
-            <button>Continue with Google</button>
-            <button>Continue with Github</button>
+            <button onClick={onSocialClick} name="google">Continue with Google</button>
+            <button onClick={onSocialClick} name="github">Continue with Github</button>
         </div>
     </div>
     )

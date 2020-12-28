86cdb826d55a3d7da8765e3f3555c698b952d4ff
김정민 <dsaqaz421@naver.com>
2020-12-28 12:13:59 +0900

    2.6 Log Out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,4 +1,17 @@
 import React from "react";
+import {authService} from "fbase";
+import {useHistory} from "react-router-dom";
 
-const Profile =  () => <span>Profile</span>
-export default Profile;
+
+export default () => {
+    const history = useHistory();
+    const onLogOutClick = () => {
+        authService.signOut();
+        history.push("/");      // redirect 태그를 안쓰고 처리
+    }
+    return (
+        <>
+            <button onClick = {onLogOutClick}>Log Out</button>
+        </>
+    );
+}

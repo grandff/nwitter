07b3ac9998bffd74647181b18add7f0468d79464
김정민 <dsaqaz421@naver.com>
2020-12-29 12:29:29 +0900

    3.2 Getting the nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { dbService } from "fbase";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");    
+    const [nweets, setNweets] = useState([]);
+    // fire base db select
+    const getNweets = async() => {
+        const dbNweets = await dbService.collection("nweets").get();
+        dbNweets.forEach((document) => {
+            const nweetObject = {
+                ...document.data(),     // spread attribute 기능.. es6 참고
+                id : document.id
+            }
+            setNweets((prev) => [nweetObject, ...prev]);      // 함수를 전달할 경우 이전 값을 불러올 수 있음 리액트의 경우
+        });        
+    }
+
+    useEffect(() => {
+        getNweets();
+    }, []);
+
     const onSubmit = async (event) => {
         event.preventDefault();
         // fire base db insert
@@ -23,6 +40,11 @@ const Home = () => {
                 <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120}/>
                 <input type="submit" value="Nweet" />
             </form>
+            <div>
+                {nweets.map((nweet) => <div key={nweet.id}>
+                    <h4>{nweet.nweet}</h4>
+                    </div>)}
+            </div>
         </div>
     );
 };

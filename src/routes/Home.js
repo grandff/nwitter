32f0b565176384243987fa5632f3dbb6ff4c5b5d
Yura Ko <dsaqaz421@naver.com>
2021-01-04 12:36:03 +0900

    4.1 Preview Images part Two

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import Nweet from "../components/Nweet"
 const Home = ( { userObj }) => {
     const [nweet, setNweet] = useState("");    
     const [nweets, setNweets] = useState([]);
+    const [attachment, setAttachment] = useState();
     // fire base db select
     // old version
     /*const getNweets = async() => {
@@ -47,10 +48,14 @@ const Home = ( { userObj }) => {
         const theFile = files[0];          
         const reader = new FileReader();
         reader.onloadend = (finishedEvent) => {
-            
+            const {currentTarget : {result}} = finishedEvent
+            setAttachment(result);
         }
         reader.readAsDataURL(theFile);
     };
+
+    // clear image file
+    const onClearAttachmentClick = () => setAttachment(null);
 
     return (
         <div>
@@ -58,6 +63,11 @@ const Home = ( { userObj }) => {
                 <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120}/>
                 <input type="file" accept="image/*" onChange={onFileChange} />
                 <input type="submit" value="Nweet" />
+                {attachment && 
+                    <div>
+                        <img src ={attachment} width = "50px" height="50px" />
+                        <button onClick={onClearAttachmentClick}>Clear</button>
+                    </div> }
             </form>
             <div>
                 {nweets.map((nweet) => (

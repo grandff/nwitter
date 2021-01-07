44fa5e82def9bbc49f785f86fd79184752bf631e
Yura Ko <dsaqaz421@naver.com>
2021-01-07 12:49:03 +0900

    5.1 Update Profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {authService, dbService} from "fbase";
 import {useHistory} from "react-router-dom";
 
 const Profile = ({userObj}) => {
     const history = useHistory();
+    const [newDisplayName, setNewDiplayName] = useState(userObj.displayName);
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/");      // redirect 태그를 안쓰고 처리
@@ -16,10 +17,29 @@ const Profile = ({userObj}) => {
 
     useEffect(() => {
         getMyNweets();
-    }, [])
+    }, []);
 
+
+    const onSubmit = async (event) => {
+        event.preventDefault();        
+        if(userObj.displayName !== newDisplayName){
+            // 프로필 사진 올리는거 추가하긔..!
+            await userObj.updateProfile({
+                displayName : newDisplayName
+            });
+        }
+    }
+
+    const onChange = (event) => {
+        const {target : {value}} = event;
+        setNewDiplayName(value);
+    }
     return (
         <>
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder="Display name" onChange={onChange} value={newDisplayName} />
+                <input type="submit" value="Update Profile" />
+            </form>
             <button onClick = {onLogOutClick}>Log Out</button>
         </>
     );

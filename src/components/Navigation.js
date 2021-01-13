deda59794b091a9e1104d49614584d0356f4048b
Yura Ko <dsaqaz421@naver.com>
2021-01-13 12:48:20 +0900

    css style 복붙만 했음.. 귀찮다 ...

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 const Navigation = ({userObj}) => <nav>
-    <ul>
+    <ul style={{ display: "flex", justifyContent: "center", marginTop: 50 }}>
         <li><Link to ="/">Home</Link></li>
         <li><Link to ="/profile">{userObj.displayName}의 Profile</Link></li>
     </ul>

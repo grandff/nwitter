6f00b72151ef33cb485052192d35b34a298d0a20
Yura Ko <dsaqaz421@naver.com>
2021-01-13 12:37:26 +0900

    6.1 Styles - AuthForm

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -8,6 +8,8 @@ const AuthForm = () => {
     const [newAccount, setNewAccount] = useState(true);
     const [error, setError] = useState("");
 
+    const inputStyles = {};
+
     const toggleAccount = () => setNewAccount((prev) => !prev);
 
     const onChange = (event) => {
@@ -40,13 +42,13 @@ const AuthForm = () => {
 
     return(
         <>
-            <form onSubmit={onSubmit}>
-                <input name="email" type="email" placeholder = "Email" required value={email} onChange={onChange}/>
-                <input name="password" type="password" placeholder = "Password" required value={password} onChange={onChange} />
-                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />        
-                {error}
+            <form onSubmit={onSubmit} className="container">
+                <input name="email" type="email" placeholder = "Email" required value={email} onChange={onChange} className="authInput"/>
+                <input name="password" type="password" placeholder = "Password" required value={password} onChange={onChange} className="authInput"/>
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} className="authInput authSubmit"/>        
+                {error && <span className="authError">{error}</span>}
             </form>
-            <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
+            <span onClick={toggleAccount} className="authSwitch">{newAccount ? "Sign In" : "Create Account"}</span>
         </>
     );
 }
